1853d9eec368cfa9c10e4ee08eafa96dcea2631b
mmoengg <mmoengg@gmail.com>
2025-01-06 14:23:56 +0900

    feat: content add component

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,12 @@
-import { auth } from '../firebase';
+import styled from 'styled-components';
+import PostTweetForm from '../components/post-tweet-form';
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-	const logOut = () => {
-		auth.signOut();
-	};
 	return (
-		<div>
-			<button onClick={logOut}>Log Out</button>
-		</div>
+		<Wrapper>
+			<PostTweetForm />
+		</Wrapper>
 	);
 }
